613924cb1cf8391e126dc92c589aad518dc0fdb8
Maela.Hurault <maela.hurault@insa-rennes.fr>
2020-02-18 22:40:35 +0100

    Add Maze

---
+++ bin/Model/Maze.java
@@ -0,0 +1,15 @@
+package bin.Model;
+
+public class Maze {
+	
+	int[][] matrix;
+
+	//Simple constructor using fields
+	public Maze(int[][] matrix) {
+		this.matrix = matrix;
+	}
+	
+	// To do : a constructor using a file
+	
+	
+}
